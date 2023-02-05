8a391a10c828aefaee3905e412de8d444de8ec74
TaejungKim <tae0803@naver.com>
2023-02-05 12:59:00 +0900

    second.commit

--- hellocpp.cpp
+++ hellocpp.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
     cout<<"hello world\n"<<endl;
-    cout<<"hello world2";
-    cout<<"안녕하세요";
+    cout<<"hello world2\n";
+    cout<<"안녕하세요\n";
+    cout<<"한글이 깨집니까?";
 }
